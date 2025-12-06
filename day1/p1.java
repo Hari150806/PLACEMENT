d6fe83daa0df82719d9b948f782e0b15faeb2fc5
Hariharan P <pchari217@gmail.com>
2025-12-06 21:39:01 +0530

    first commit

---
+++ day1/p1.java
@@ -0,0 +1,16 @@
+package day1;
+import java.util.*;
+
+public class p1 {
+    public static void printTable(int n,int i,int j){
+        if(i > j){
+            return;
+        }
+        System.out.println(n+" * "+i+" = "+(n*i));
+        printTable(n, i + 1, j);   // increment i
+    }
+
+    public static void main(String[] args){
+        printTable(8, 3, 18);
+    }
+}
